ecf9535f7cdcc805984b2b790980f42e8cfbf1f7
BaseSummary <1439927138@qq.com>
2018-10-26 16:03:43 +0800

    添加了新的文件

---
+++ test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by 51pgzs on 2018/10/26.
+//  Copyright © 2018年 xuehuaqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
